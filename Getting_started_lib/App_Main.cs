e4f99238aa105cd1a3fdf5b2dc7d4022af1cd796
VM <digvijay.taunk@gmail.com>
2021-12-20 23:53:43 +0530

    Add namespace and make class static

---
+++ Getting_started_lib/App_Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using NXOpen;
+
+namespace Getting_started_lib
+{
+    public static class App_Main
+    {
+   
+        public static void Main()
+        {
+            Session theSession = Session.GetSession();
+            Part part = theSession.Parts.Work;
+            MessageBox.Show("Full Path " + part.FullPath, "Demo");
+            double mass = PartProperties.GetPartWeigth(part);
+            MessageBox.Show("Mass is = " + mass.ToString(), "Mass Properties"); 
+        }
+
+        /// <summary>
+        /// Must have function to unload dll after execution.
+        /// </summary>
+        /// <param name="dummy"></param>
+        /// <returns></returns>
+        public static int GetUnloadOption(String dummy)
+        {
+            return (int)Session.LibraryUnloadOption.Immediately;
+        }
+
+    }
+}
